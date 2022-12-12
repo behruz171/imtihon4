9fc03dce38a8237629d9b7a8e207207608d4e7ba
behruz171 <behruzzo662@gmail.com>
2022-12-12 21:34:52 +0500

    imtihon bajarildi

---
+++ js/massiv.js
@@ -0,0 +1,66 @@
+massiv = [
+    {
+        name:"Jane Cooper",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"jone@microsoft.com",
+        guruh:"Frontend 010",
+        status:"false"
+    },
+    {
+        name:"Behruz",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"behruz@microsoft.com",
+        guruh:"Frontend 010",
+        status:"true"
+    },
+    {
+        name:"Stif",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"stif@microsoft.com",
+        guruh:"Frontend 010",
+        status:"false"
+    },
+    {
+        name:"Rayhon",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"rayhon@microsoft.com",
+        guruh:"Frontend 010",
+        status:"true"
+    },
+    {
+        name:"Fozil",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"fozil@microsoft.com",
+        guruh:"Frontend 010",
+        status:"true"
+    },
+    {
+        name:"Sanobar",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"sanobar@microsoft.com",
+        guruh:"Frontend 010",
+        status:"false"
+    },
+    {
+        name:"Otkir",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"otkir@microsoft.com",
+        guruh:"Frontend 010",
+        status:"true"
+    },
+    {
+        name:"Hojakbar",
+        ish:"Microsoft",
+        tel:"(225) 555-o118",
+        email:"hojakbar@microsoft.com",
+        guruh:"Frontend 010",
+        status:"true"
+    },
+]
